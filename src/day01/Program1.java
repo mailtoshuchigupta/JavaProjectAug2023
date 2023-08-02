d512492771fef4549a8f8b28e5f45fc74b4d4af3
mailtoshuchigupta <140995434+mailtoshuchigupta@users.noreply.github.com>
2023-08-02 12:34:17 +1000

    Update Program1.java

--- src/day01/Program1.java
+++ src/day01/Program1.java
@@ -5,6 +5,7 @@ public class Program1 {
 	public static void main(String[] args) {
               System.out.println("Good Day");
 		System.out.println("Good Day");
+		System.out.println("Good Day");
 	}
 
 }
