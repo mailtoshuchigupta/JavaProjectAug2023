7aaa18532fc3b68f479a3c7a43ec9f0f667b2759
mailtoshuchigupta <140995434+mailtoshuchigupta@users.noreply.github.com>
2023-08-02 13:16:36 +1000

    day01
    sample

--- src/day01/Program1.java
+++ src/day01/Program1.java
@@ -3,9 +3,9 @@ package day01;
 public class Program1 {
 
 	public static void main(String[] args) {
-              System.out.println("Good Day");
+		// TODO Auto-generated method stub
 		System.out.println("Good Day");
-		System.out.println("Good Day");
+		System.out.println("G Day");
 	}
 
 }
